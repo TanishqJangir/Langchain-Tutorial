0b08854d5a1a7c3d5e3fa85409a9207093adbaeb
TanishqJangir <tanishqjangir001@gmail.com>
2026-06-05 21:24:09 +0530

    huggingace init

---
+++ 2_ChatModels/4_Chatmodel_hf_api.py
@@ -0,0 +1,13 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
+    task="text-generation"
+)
+
+model = ChatHuggingFace(llm=llm)
+result = model.invoke("Write a 5 line poem on Tanishq")
+print(result.content)
