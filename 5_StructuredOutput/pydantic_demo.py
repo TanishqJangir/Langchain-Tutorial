68e3be6e9ef593d505e36571ca481b591278af7e
TanishqJangir <tanishqjangir001@gmail.com>
2026-06-12 20:24:35 +0530

    with structured output pydantic

--- 5_StructuredOutput/pydantic_demo.py
+++ 5_StructuredOutput/pydantic_demo.py
@@ -1,9 +1,18 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr, Field
+from typing import Optional
 
 class Student(BaseModel):
     name : str
-    age : int
+    age : Optional[int] = None
+    email: EmailStr
+    cgpa : float = Field(gt=0, lt=10, default=4, description="A decimal Value representing the cgpa of the student.")
 
-new_student = { "name": "Tanishq", "age": 21}
+new_student = {"name" :"Tanishq", "age":22, "email":"tanishqjangir@gmail.com"}
 student = Student(**new_student)
-print(student)
+student_dict = dict(student)
+
+print(student_dict)
+
+student_json = student.model_dump_json()
+
+print(student_json)
---
+++ 5_StructuredOutput/with_structured_output_pydantic.py
@@ -0,0 +1,40 @@
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+from typing import TypedDict, Annotated, Optional, Literal
+from pydantic import BaseModel, Field
+
+load_dotenv()
+
+model = ChatOpenAI(model="gpt-4o-mini")
+
+class Review(BaseModel):
+
+    key_themes : list[str] = Field(description="Write down all the key themes discussed in the review in a list.")
+    summary : str = Field(description="Summary of the review")
+    sentiment : Literal["Positive","Negative","Neutral"] = Field(description="Sentiment of the review : Positive,Negative,Neutral")
+    rating : float = Field(description="Rating of the review : 1-5")
+    pros: Optional[list[str]] = Field(default=None, description="Write down all the pros inside the list")
+    cons: Optional[list[str]] = Field(default=None, description="Write down all the cons inside the list")
+    name: Optional[str] = Field(default=None, description="Name of the reviewer")
+
+structured_output  = model.with_structured_output(Review)
+
+result = structured_output.invoke("""I recently upgraded to the Samsung Galaxy S24 Ultra, and I must say, it's an absolute powerhouse! The Snapdragon 8 Gen 3 rocessor makes everything lightning fast—whether I'm gaming, multitasking, or asts a full dav even with heavv use. and the 45W fast charging is a lifesaver.
+
+The S-Pen integration is a great touch for note-taking and quick sketches, though I don't use it often. What really blew me away is the 20MP camera—the night mode is stunning, capturing crisp, vibrant images even in low light. Zooming up to leex actually works well for distant objects, but anything beyond loses quality.
+
+However, the weight and size make it a bit uncomfortable for one-handed use. Also, Samsung's One UI still comes with bloatware—why do I need five different Samsung apps for things Google already provides? The $1,30 price tag is also a hard pill to swallow.
+
+pros :
+Insanely powerful processor (great for gaming and productivity)
+Stunning 200MP camera with incredible zoom capabilities
+Long battery life with fast charging
+S-Pen support is unique and useful
+
+Cons :
+Bulky and heavy—not great for one-handed use
+Bloatware still exists in One UI
+Expensive compared to competitors
+""")
+
+print(result)
