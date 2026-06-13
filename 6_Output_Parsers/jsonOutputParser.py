4af7d0e285fbfd4c471834b327779f4d132ac640
TanishqJangir <tanishqjangir001@gmail.com>
2026-06-13 21:46:11 +0530

    json output parser

---
+++ 6_Output_Parsers/jsonOutputParser.py
@@ -0,0 +1,29 @@
+from langchain_core.output_parsers import JsonOutputParser
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id="meta-llama/Meta-Llama-3-8B-Instruct",
+    task="text-generation"
+)
+
+model = ChatHuggingFace(llm=llm)
+
+parser = JsonOutputParser()
+
+template = PromptTemplate(
+    template="Give me 5 facts about {topic} \n {format_instruction}",
+    input_variables=['topic'],
+    partial_variables={
+        "format_instruction": parser.get_format_instructions()
+    } 
+)
+
+chain = template | model | parser
+
+result = chain.invoke({'topic':'Black Hole'})
+
+print(result) 
