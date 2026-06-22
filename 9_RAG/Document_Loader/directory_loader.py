220edf70cc60718b0c3e1a00bc7ce396372b6ee8
TanishqJangir <tanishqjangir001@gmail.com>
2026-06-22 23:09:55 +0530

    directory_loader

---
+++ 9_RAG/Document_Loader/directory_loader.py
@@ -0,0 +1,15 @@
+from langchain_community.document_loaders import DirectoryLoader
+from langchain_community.document_loaders import PyPDFLoader
+
+loader = DirectoryLoader(
+    path='books',   #here is the path of the directory
+    glob='*.txt',   #here is the glob pattern
+    show_progress=True,#to show the progress of the loader
+    loader_cls=PyPDFLoader
+)
+
+
+#load the documents
+docs = loader.load()
+
+print(docs)
