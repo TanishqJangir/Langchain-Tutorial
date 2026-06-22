2ce82d8d4bffc1ada1c31460c88327567c8de0b9
TanishqJangir <tanishqjangir001@gmail.com>
2026-06-22 12:31:34 +0530

    text_loader

---
+++ 9_RAG/Document_Loader/text_loader.py
@@ -0,0 +1,26 @@
+from langchain_community.document_loaders import TextLoader
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
+
+load_dotenv()
+
+model = ChatOpenAI()
+
+prompt = PromptTemplate(
+    template='Write a summary for the following text - \n {text}',
+    input_variables=['text']
+)
+
+parser = StrOutputParser()
+
+loader = TextLoader("9_RAG\\Document_Loader\\cricket.txt", encoding="utf-8")
+
+docs = loader.load()
+
+chain = prompt | model | parser
+
+result = chain.invoke({'text' : docs[0].page_content})
+
+print(result)
