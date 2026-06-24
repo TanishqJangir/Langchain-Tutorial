e84cc3de41d5f59109679a9599d3b31f040772d1
Tanishq Jangir <tanishqjangir001@gmail.com>
2026-06-25 00:20:26 +0530

    new bign

---
+++ 9_RAG/Text_Splitter/length_based.py
@@ -0,0 +1,29 @@
+from langchain_classic.text_splitter import CharacterTextSplitter
+from langchain_community.document_loaders import PyPDFLoader
+
+loader = PyPDFLoader('9_RAG\\Text_Splitter\\AI.pdf')
+
+docs = loader.load()
+
+
+text = """
+Snace exploration has Jed to incredlbie scientific discoveres.
+From On the Moon to exportng Mars, humanity
+continues to push the boundanes of what's possible beyond our
+planet.
+missions have not expanded our knowledge Of the
+universe but have also contributed to advancements in
+technology here on Earth. Satellite communications, GPS, and
+even certain medical imaging techniques trace the" roots back
+to innovations driven by space programs.
+"""
+
+splitter = CharacterTextSplitter(
+    chunk_size=100,
+    chunk_overlap=0,
+    separator=''
+)
+
+result = splitter.split_text(text)
+
+print(result)
---
+++ 9_RAG/Text_Splitter/text_structure_based.py
@@ -0,0 +1,24 @@
+from langchain_classic.text_splitter import RecursiveCharacterTextSplitter
+
+text = """
+Snace exploration has Jed to incredlbie scientific discoveres.
+From On the Moon to exportng Mars, humanity
+continues to push the boundanes of what's possible beyond our
+planet.
+missions have not expanded our knowledge Of the
+universe but have also contributed to advancements in
+technology here on Earth. Satellite communications, GPS, and
+even certain medical imaging techniques trace the" roots back
+to innovations driven by space programs.
+"""
+
+
+splitter = RecursiveCharacterTextSplitter(
+    chunk_size=100,
+    chunk_overlap=0,
+    separators=['\n\n', '\n', ' ']
+)
+
+result = splitter.split_text(text)
+
+print(result)
